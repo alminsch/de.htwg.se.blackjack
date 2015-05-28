69d384504f2d59b48f87ab8f579dd06dedb5c2a3
Benjamin Herrmann <conquiztadoracc@web.de>
2015-05-28 15:48:55 +0200

    added TextUI

--- de.htwg.se.blackjack/src/de/htwg/blackjack/view/tui/TextUI.java
+++ de.htwg.se.blackjack/src/de/htwg/blackjack/view/tui/TextUI.java
@@ -1,5 +1,26 @@
 package de.htwg.blackjack.view.tui;
 
-public class TextUI {
+import de.htwg.blackjack.controller.Controller;
 
+public class TextUI {
+	Controller controller;
+	public TextUI(Controller controller) {
+        this.controller = controller;
+    }
+	public boolean userinputselection(String next) {
+		boolean go = true;
+		switch (next) {
+		case"q":
+			go = false;
+			break;
+		case"s":
+			//Controller
+			break;
+		case"b":
+			//Controller
+
+		}
+
+		return go;
+	}
 }
